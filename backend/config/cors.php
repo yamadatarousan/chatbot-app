ecbefbe9214abcb750e3380ae6ac7d722f50e57e
sohei.kakihara <souhei_kakihara@simple-inc.com>
2025-05-05 16:06:56 +0900

    リクエスト時に発生していたcorsエラー回避対応

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,6 +1,12 @@
 <?php
 
 return [
-    'paths' => ['api/*'],
-    'allowed_origins' => ['http://localhost:3000'],
+    'paths' => ['api/*'], // /api/* に適用
+    'allowed_methods' => ['*'], // GET, POST など全部許可
+    'allowed_origins' => ['http://localhost:3001'], // フロントエンドのURL
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'], // Content-Type とか全部許可
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => false,
 ];
